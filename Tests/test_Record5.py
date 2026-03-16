2ded0d7bf4d9de57267608e6b3824617851e4bd4
Chester <chester.reyes101028@gmail.com>
2026-03-17 00:29:47 +0800

    Hello again x7

--- Tests/test_Record5.py
+++ Tests/test_Record5.py
@@ -2,7 +2,7 @@ import os
 from playwright.sync_api import Page, expect
 from Pages.Login_Page import LoginPage
 from dotenv import load_dotenv
-load_dotenv()
+load_dotenv(override=True)
 
 
 def test_example(page: Page):
--- Tests/testenv.py
+++ Tests/testenv.py
@@ -2,7 +2,7 @@ from encodings.punycode import T
 import os
 from dotenv import load_dotenv,dotenv_values
 
-dotenv_values()
+load_dotenv(override=True)
 
 print(os.getenv("Base_URL"))
 print(os.getenv("Username"))
